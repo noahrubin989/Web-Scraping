62eb9eca77cce4fdbfc7242f663500632c462fa1
noahrubin989 <77037165+noahrubin989@users.noreply.github.com>
2023-01-23 13:31:41 +1100

    Add files via upload

---
+++ web_scraping/basketball_scraper_advanced/app.py
@@ -0,0 +1,15 @@
+# Script to scrape basketball statistics
+# Noah Rubin 2023
+
+from helpers import scrape_bball_stats
+
+
+def main():
+    start = 2002
+    end = 2023
+    driver_path = '/Users/noahrubin/Desktop/chromedriver'
+    data = scrape_bball_stats(start, end, driver_path=driver_path)
+    data.to_csv('../datasets/basketball/basketball_data.csv', index=False)
+
+if __name__ == '__main__':
+    main()
